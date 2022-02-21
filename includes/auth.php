b7be76f5d25ed5bd3d8db062fee548c83a5e8c24
littleOkittenOwantsOsugar <topevol33@gmail.com>
2022-02-21 15:58:57 +0300

    To do list

--- includes/auth.php
+++ includes/auth.php
@@ -8,8 +8,24 @@
     $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$pass'");
     
     if (mysqli_num_rows($check_user) > 0){
+
+        $user= mysqli_fetch_assoc($check_user);
+
+        $_SESSION['user']=[
+            "id" => $user['id'],
+            "login" => $user['login']
+        ];
+
         
+
     } else{
         $_SESSION['message'] = 'Wrong login or password';
         header('Location: ../index.php');
     }
+    ?>
+<pre>
+    <?php
+    print_r($check_user);
+    print_r($user);
+    ?>
+</pre>
