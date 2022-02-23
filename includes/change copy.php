acb2e644d02bdfdc9d3b3be41714a817cfa353cc
littleOkittenOwantsOsugar <topevol33@gmail.com>
2022-02-24 01:30:11 +0300

    :(

--- includes/change copy.php
+++ includes/change copy.php
@@ -8,16 +8,20 @@
 
     //НЕ ОПРЕДЕЛЯЕТ id
 
-    $idq=$_REQUEST['id'];
+    result_select=mysqli_query($con, "SELECT * FROM `registration`.`listtodo`");
+    $row = mysqli_fetch_array($result_select);
+    $id=$row['id'];
+
+    //$idq = $_REQUEST['id'];
     $name=$_SESSION['user']['id'];
     $text=$_POST["mes"]; 
 
-    $s = "REPLACE INTO `listtodo` SET `id` = '".$idq."', `iduser` = '".$name."', `message` = '".$text."'";
+    $s = "REPLACE INTO `listtodo` SET `id` = '".$id."', `iduser` = '".$name."', `message` = '".$text."'";
 
     $_SESSION['message'] = $s;
 
     mysqli_query($con, $s);
 
-    header('Location: ../listtodo.php');
+    //header('Location: ../listtodo.php');
 
 ?>
